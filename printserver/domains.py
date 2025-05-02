20f5d86b846c7d6fba6f4f07842932cedb95a4f0
Samuel Hoffstaetter <sam@shortstorybox.com>
2025-05-02 14:42:33 -0700

    fix: allowlist localhost (#15)

--- printserver/domains.py
+++ printserver/domains.py
@@ -81,6 +81,8 @@ class AllowDomainMiddleware:
             # and doesn't send the Origin header while relying on CORS for
             # access control.
             pass
+        elif origin == request.forwarded_prefix:
+            pass # Allow requests from the printserver itself
         elif not self.format_is_valid(origin):
             raise falcon.HTTPBadRequest(
                 description=f"Origin header cannot be parsed: {repr(origin)}"
