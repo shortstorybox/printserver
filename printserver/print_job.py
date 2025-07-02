9da2ab1d78ced8b982006de2b33dffe33171c90e
sooham <neel@shortstorybox.com>
2025-07-02 13:21:31 -0400

    fix: update

--- printserver/print_job.py
+++ printserver/print_job.py
@@ -188,9 +188,9 @@ class ListPrintJobApi:
         elif not isinstance(units_param, str):
             raise ValueError(f"Invalid value for mediaSize.units: {repr(units_param)}")
         else:
-            if units_param.upper() in SizeUnit.__members__:
-                units = SizeUnit[units_param.upper()]
-            else:
+            try:
+                units = SizeUnit(units_param.lower())
+            except ValueError:
                 raise ValueError(
                     f"Invalid value for mediaSize.units: {repr(units_param)}. Must be one of: {', '.join([unit.value for unit in SizeUnit])}"
                 )
